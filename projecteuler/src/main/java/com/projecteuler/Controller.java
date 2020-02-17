f5e46f277cee5547a52a81be3d22d93785ff0be5
Seth Broughton <broughton.24@gmail.com>
2020-02-17 11:28:03 -0500

    WIP

--- projecteuler/src/main/java/com/projecteuler/Controller.java
+++ projecteuler/src/main/java/com/projecteuler/Controller.java
@@ -4,7 +4,7 @@ public class Controller {
 
 	public static void main(String[] args) {
 		 
-		System.out.print(new LargestProductGrid().product());
+		System.out.print(new Triangular().number());
 		
 	}
 
---
+++ projecteuler/src/main/java/com/projecteuler/TriangularNumber.java
@@ -0,0 +1,11 @@
+package com.projecteuler;
+
+public class TriangularNumber {
+	
+	public int number() {
+		
+		
+		
+	}
+
+}
